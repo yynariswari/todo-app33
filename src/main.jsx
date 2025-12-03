f7b0e57901023ec94b431f07879945b5142a4ef5
Yuniar Nariswari <yuniarnariswari@gmail.com>
2025-12-03 14:58:18 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ⬅️ diganti
 
 import { TodoProvider } from "./context/TodoContext.jsx";
 import { VoteProvider } from "./context/VoteContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
+      {/* ⬅️ diganti */}
       <TodoProvider>
         <VoteProvider>
           <App />
         </VoteProvider>
       </TodoProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
